e488ec3e116709c103b1b5e0101b117633639ec1
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-16 06:19:20 +0900

    edit,updateアクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :email, :age, :introduction))
+      flash[:notice] = "ユーザーIDが「#{@user.id}」の情報を更新しました"
+      redirect_to :users
+    else
+      render "edit"
+    end
+  end
 end
