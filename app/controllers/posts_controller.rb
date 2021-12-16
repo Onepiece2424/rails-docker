61732880d522aa557ad11ed5251a961b70eb49c9
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-16 20:49:59 +0900

    投稿機能の参照・編集・削除機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,14 +16,27 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:content, :user_id))
+      flash[:notice] = "投稿を更新しました"
+      redirect_to :users
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to :users
   end
 end
