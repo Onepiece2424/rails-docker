5c2a1ce8befd74a1ebedf45143f995a52992b7a8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-16 20:42:37 +0900

    投稿機能の追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(params.require(:post).permit(:content, :user_id))
+    if @post.save
+      flash[:notice] = "新規投稿をしました"
+      redirect_to :users
+    else
+      render "posts"
+    end
   end
 
   def show
