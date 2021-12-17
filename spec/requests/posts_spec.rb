d2b3c76088c8609a5d91b30634997f4e6826fe1d
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-18 07:34:07 +0900

    rspecのコメントアウト（後に実装）

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,18 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+# RSpec.describe "投稿ページ", type: :request do
+#   let(:post) { create(:post) }
+#   let(:user) { create(:user) }
+#   let(:post_user) { create(:post, user: user)}
+#   before do
+#     get post_url post.id
+#   end
 
-end
+#   it 'レスポンスが返ってくること' do
+#     expect(response.status).to eq(200)
+#   end
+
+#   it 'レスポンスに投稿者名が含まれること' do
+#     expect(response.body). to include post_user.name
+#   end
+# end
