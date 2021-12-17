6596d76ba9cd4b52fa65c917ca94da489fba58c8
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2021-12-17 20:44:14 +0900

    不要なインデントの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.new
-    @posts = @user.posts 
+    @posts = @user.posts
   end
 
   def edit
